e60364a70870227255e53849410c5c4b99b718f7
yangroro <yang@heechan.kr>
2020-08-15 13:49:23 +0900

    Add MDD cumulative_yield

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -16,8 +16,14 @@ class Evaluator:
     def _add_profit_rate(pnl: DataFrame) -> DataFrame:
         """이익율 컬럼 추가"""
         with_profit = pnl.copy()
-        with_profit['profit_rate'] = pnl.profit / pnl.shift(1).pnl * 100
+        with_profit['profit_rate'] = pnl.profit / pnl.shift(1).pnl
         return with_profit
+
+    @staticmethod
+    def _get_cumulative_yield(pnl: DataFrame) -> DataFrame:
+        """누적수익율"""
+        cumulative_yield = (pnl['pnl'].iloc[-1] - pnl['pnl'].iloc[0]) / pnl['pnl'].iloc[0]
+        return cumulative_yield
 
     @staticmethod
     def _get_winning_rate(pnl: DataFrame) -> (float, int):
@@ -35,11 +41,23 @@ class Evaluator:
         profit_pnl = pnl[pnl.profit_rate > 0]
         loss_pnl = pnl[pnl.profit_rate < 0]
         rate = (profit_pnl['profit_rate'].sum() / len(profit_pnl)) / (
-                abs(loss_pnl['profit_rate'].sum()) / len(loss_pnl))
+            abs(loss_pnl['profit_rate'].sum()) / len(loss_pnl))
         return rate
 
     @staticmethod
+    def _add_mdd(pnl: DataFrame) -> DataFrame:
+        """최대 낙폭"""
+        tmp = pnl.copy()
+        with_mdd = pnl.copy()
+        tmp['ath'] = tmp.rolling(len(tmp), min_periods=1)['pnl'].max()
+        tmp['dd'] = tmp['pnl'] - tmp['ath']
+        with_mdd['mdd'] = tmp.rolling(len(tmp), min_periods=1)['dd'].min() / tmp['ath']
+
+        return with_mdd
+
+    @staticmethod
     def _get_shape_ratio(pnl: DataFrame) -> float:
+        """샤프 지수"""
         return pnl['profit_rate'].mean() / pnl['profit_rate'].std()
 
     @staticmethod
@@ -53,7 +71,7 @@ class Evaluator:
         """
         PNL 받아서 알파 성능 측정
         :param pnl: DataFrame columns: date, pnl
-        :return: 거래일수, 승률, 손익비, 샤프지수
+        :return: 거래일수, 승률, 손익비, 샤프지수, 최대 낙폭 퍼센트, 누적수익율
         """
 
         ### 수정 사항 ---> 연도 별로 pnl 평가하도록 하자...! (total과 병행....!)
@@ -67,6 +85,7 @@ class Evaluator:
 
         pnl = cls._add_profit(pnl)
         pnl = cls._add_profit_rate(pnl)
+        pnl = cls._add_mdd(pnl)
         pnl = pnl[1:]
         raw_pnl = pnl.copy()
         pnl = cls._remove_non_trading_days(pnl)
@@ -77,20 +96,26 @@ class Evaluator:
             mask = (pnl.index >= start) & (pnl.index < end)
             this_year_df = pnl[mask]
             winning_rate, trading_days = cls._get_winning_rate(this_year_df)
+            mdd = this_year_df['mdd'].min()
             year_dict = {
                 'trading_days': trading_days,
                 'winning_rate': winning_rate,
                 'profit_loss_rate': cls._get_profit_loss_rate(this_year_df),
                 'shape_ratio': cls._get_shape_ratio(this_year_df),
+                'mdd': mdd,
+                'cumulative_yield': cls._get_cumulative_yield(this_year_df),
             }
             result[year] = year_dict
 
         winning_rate, trading_days = cls._get_winning_rate(pnl)
+        mdd = pnl['mdd'].min()
         result['total'] = {
             'trading_days': trading_days,
             'winning_rate': winning_rate,
             'profit_loss_rate': cls._get_profit_loss_rate(pnl),
             'shape_ratio': cls._get_shape_ratio(raw_pnl),
+            'mdd': mdd,
+            'cumulative_yield': cls._get_cumulative_yield(pnl),
         }
         year_df = DataFrame.from_dict(result, orient='index')
         return year_df
