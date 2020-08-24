be5df39f5b77d247b31f2b92719db9e8c214cf28
yangroro <yang@heechan.kr>
2020-08-24 14:18:41 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='utils',
-    version='3.1.0',
+    version='3.1.1',
     description='Alpha trading utils with pandas',
     python_requires='==3.*,>=3.7.0',
     project_urls={"homepage": "https://github.com/alpha-trading/utils", "repository": "https://github.com/alpha-trading/utils"},
