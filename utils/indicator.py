2516d0c2b3cc925e9bf62d0680625e9ea1151246
yangroro <yang@heechan.kr>
2020-09-02 16:49:32 +0900

    Change type of methods

--- utils/indicator.py
+++ utils/indicator.py
@@ -7,40 +7,38 @@ import numpy as np
 
 class Indicator:
     @staticmethod
-    def get_sma(df: DataFrame, day: int, column='close') -> Series:
+    def get_sma(price: Series, day: int) -> Series:
         """
         단순 이동평균선(평균값으로) 구하기
         Example: add_sma(df, 10)
-        :param column:
-        :param df:
+        :param price:
         :param day: 일자별 이동평균선 구하기
         :return:
         """
-        return df[column].rolling(window=day).mean()
+        return price.rolling(window=day).mean()
 
     @staticmethod
-    def get_ema(df: DataFrame, day: int, column='close') -> Series:
+    def get_ema(price: Series, day: int) -> Series:
         """
         지수 이동평균선(최근일에 가중치를 주는 방식) 구하기
         Example: add_ema(df, 5)
-        :param column:
-        :param df:
+        :param price:
         :param day:
         :return:
         """
-        return df[column].ewm(span=day, adjust=False).mean()
+        return price.ewm(span=day, adjust=False).mean()
 
     @staticmethod
-    def get_rsi(df: DataFrame, day: int) -> Series:
+    def get_rsi(price: Series, day: int) -> Series:
         """
         RSI 과매도 과매수 판단 지표
         Example add_rsi(df, 14)
-        :param df:
+        :param price:
         :param day:
         :return:
         """
-        up = np.where(df['close'].diff(1) > 0, df['close'].diff(1), 0)
-        down = np.where(df['close'].diff(1) < 0, df['close'].diff(1) * (-1), 0)
+        up = np.where(price.diff(1).gt(0), price.diff(1), 0)
+        down = np.where(price.diff(1).lt(0), price.diff(1).multiply(-1), 0)
 
         average_up = Series(up).rolling(window=day, min_periods=day).mean()
         average_down = Series(down).rolling(window=day, min_periods=day).mean()
@@ -57,25 +55,25 @@ class Indicator:
         return (df['close'] - df['low']) / (df['high'] - df['low'])
 
     @staticmethod
-    def get_stddev(df: DataFrame, day: int, ddof: int = 1) -> Series:
+    def get_stddev(price: Series, day: int, ddof: int = 1) -> Series:
         """
-        종가 기준으로 표준편차 구하기
+        가격 기준으로 표준편차 구하기
         Example add_stddev(df, 5)
-        :param df:
+        :param price:
         :param day:
         :param ddof:
         :return:
         """
-        return df['close'].rolling(window=day).std(ddof=ddof)
+        return price.rolling(window=day).std(ddof=ddof)
 
     @classmethod
-    def get_bollinger(cls, df: DataFrame, day: int, r: int) -> Tuple[Series, Series]:
+    def get_bollinger(cls, price: Series, day: int, r: int) -> Tuple[Series, Series]:
         """
         볼린저 밴드
         Example: Indicator.add_bollinger(df, 20, 2)
         """
-        line_mid = cls.get_sma(df, day)
-        line_std = cls.get_stddev(df, day, ddof=0)
+        line_mid = cls.get_sma(price, day)
+        line_std = cls.get_stddev(price, day, ddof=0)
 
         bollinger_range = line_std.multiply(r)
         upper = line_mid + bollinger_range
@@ -84,12 +82,12 @@ class Indicator:
         return upper, lower
 
     @classmethod
-    def get_envelope(cls, df: DataFrame, day: int, r: float) -> Tuple[Series, Series]:
+    def get_envelope(cls, price: Series, day: int, r: float) -> Tuple[Series, Series]:
         """
         sma + upper and lower bounds
         Example: add_envelope(df, 20, 0.05)
         """
-        line_mid = cls.get_sma(df, day)
+        line_mid = cls.get_sma(price, day)
 
         envelope_range = line_mid.multiply(r)
         upper = line_mid + envelope_range
@@ -159,12 +157,12 @@ class Indicator:
         return demark_high, demark_low
 
     @staticmethod
-    def get_momentum(df: DataFrame, day: int) -> Series:
-        return df['close'].diff(day) / df['close'].shift(day)
+    def get_momentum(price: Series, day: int) -> Series:
+        return price.diff(day) / price.shift(day)
 
     @staticmethod
-    def get_psychological_line(df: DataFrame, day: int) -> Series:
-        up = np.where(df['close'].diff(1) > 0, 1, 0)
+    def get_psychological_line(price: Series, day: int) -> Series:
+        up = np.where(price.diff(1).gt(0), 1, 0)
         sum_up = Series(up).rolling(window=day, min_periods=day).sum()
 
         psychological = sum_up.divide(day)
@@ -173,7 +171,7 @@ class Indicator:
 
     @classmethod
     def get_disparity(cls, df: DataFrame, day: int) -> Series:
-        sma = cls.get_sma(df, day)
+        sma = cls.get_sma(df['close'], day)
         disparity = df['close'].divide(sma)
 
         return disparity
