479df4a72bc852c7cd70165c9701222ad2bf16a2
Dongkyoo <wind.dong.dream@gmail.com>
2021-03-08 23:40:27 +0900

    get_simulating_data 구현

--- utils/generator.py
+++ utils/generator.py
@@ -1,27 +1,26 @@
-from pypika import MySQLQuery, Table, Tables, Criterion
+from pypika import MySQLQuery, Table, Tables, Criterion, Field
 from typing import Optional
 
 from utils import Executor
-from datetime import date
+import datetime
 from pandas import DataFrame
 from utils.parameter import Universe, ExchangeRate
-
-TABLE_RAW_CANDLE_DAY = "data_candleday"
-TABLE_EDITED_CANDLE_DAY = "data_editedcandleday"
+from utils.parameter import Table as PhysicalTable
+from utils.parameter import Field as PhysicalField
 
 
 class Generator:
     def __init__(self, executor: Executor):
         self.executor = executor
 
-    def get_trading_day_list(self, start_date: date, end_date: date) -> list:
+    def get_trading_day_list(self, start_date: datetime.datetime.date, end_date: datetime.datetime.date) -> list:
         """
         과거 거래일 리스트를 조회하는 함수
         :param start_date: back test 시작일
         :param end_date: back test 종료일
         :return: 과거 거래일 리스트 반환
         """
-        data_trading_day = Table("data_iskoreatradingday")
+        data_trading_day = Table(PhysicalTable.is_korea_trading_day.value)
         query = (
             MySQLQuery.from_(data_trading_day)
             .select(data_trading_day.date)
@@ -40,41 +39,65 @@ class Generator:
         trading_day_list = df["date"].to_list()
         return trading_day_list
 
+    def get_simulating_data(self, universe: Universe, field_list: list, start_date: datetime.date, end_date: datetime.date):
+        ticker_history_table = Table(PhysicalTable.ticker_history.value)
+        candle_table = Table(PhysicalTable.candle_day.value)
+        trading_trend_table = Table(PhysicalTable.day_trading_trend.value)
+        trading_info_table = Table(PhysicalTable.day_trading_info.value)
+        exchange_rate_table = Table(PhysicalTable.exchange_rate_candle_day.value)
+
+        query = MySQLQuery.from_(ticker_history_table).select('date').where(
+            Criterion.all(
+                [ticker_history_table.date >= start_date, ticker_history_table.date <= end_date, ticker_history_table.is_active]
+            )
+        )
+
+        if universe != Universe.total:
+            query = query.where(
+                Criterion.all(
+                    [ticker_history_table.universe == universe]
+                )
+            )
+
+        # candle_day 를 요구
+        if any([x.table == PhysicalTable.candle_day for x in field_list]):
+            query = query.join(candle_table).using('date', 'ticker_id')
+
+        # trading_trend 를 요구
+        if any([x.table == PhysicalTable.day_trading_trend for x in field_list]):
+            query = query.join(trading_trend_table).using('date', 'ticker_id')
+
+        # trading_info 를 요구
+        if any([x.table == PhysicalTable.day_trading_info for x in field_list]):
+            query = query.join(trading_info_table).using('date', 'ticker_id')
+
+        # 환율 정보
+        for column in ExchangeRate:
+            query.join(exchange_rate_table).on(
+                Criterion.all(
+                    [exchange_rate_table.date == ticker_history_table.date, exchange_rate_table.ticker == column]
+                )
+            )
+
+        query = query.select(*[Field(x.column_name, table=Table(x.table.value)) for x in field_list])
+        df = self.executor.sql(query.get_sql())
+        return df
+
     def get_past_universe_stock_list(self, universe: Universe) -> list:
         """
         과거에 한번이라도 universe에 포함된 종목 리스트 반환
-        :param universe: kospi, kosdaq, kospi200, kosdaq150, top2000, top350(kospi200과 kosdaq150)
+        :param universe: total, kospi, kosdaq
         :return: universe에 포함되었던 종목 리스트 반환
         """
-        if universe in (Universe.total, Universe.kospi, Universe.kosdaq):
-            data_ticker = Table("data_ticker")
-            if universe == Universe.total:
-                query = MySQLQuery.from_(data_ticker).groupby(data_ticker.ticker).select("*")
-            else:
-                query = (
-                    MySQLQuery.from_(data_ticker)
-                    .groupby(data_ticker.id)
-                    .having(data_ticker.market == universe.name)
-                    .select("*")
-                )
-            df = self.executor.sql(query.get_sql())
-            past_universe_stock_list = df["ticker"].to_list()
+        data_ticker = Table(PhysicalTable.ticker.value)
 
-            return past_universe_stock_list
+        query = MySQLQuery.from_(data_ticker).select("ticker_id").distinct()
+        if universe != Universe.total:
+            query = query.where(data_ticker.universe == universe)
+        df = self.executor.sql(query.get_sql())
+        past_universe_stock_list = df["ticker"].to_list()
 
-        elif universe in (Universe.top350, Universe.kospi200, Universe.kosdaq150):
-            if universe == Universe.top350:
-                kospi200_past_universe_stock_list = self.get_past_universe_stock_list(Universe.kospi200)
-                kosdaq150_past_universe_stock_list = self.get_past_universe_stock_list(Universe.kosdaq150)
-                return kospi200_past_universe_stock_list + kosdaq150_past_universe_stock_list
-            elif universe in (Universe.kospi200, Universe.kosdaq150):
-                data_universe = Table(f"data_{universe.name}")
-                query = (
-                    MySQLQuery.from_(data_universe).groupby(data_universe.ticker_id).select(data_universe.ticker_id)
-                )
-                df = self.executor.sql(query.get_sql())
-                past_universe_stock_list = df["ticker_id"].to_list()
-                return past_universe_stock_list
+        return past_universe_stock_list
 
     def __get_day_price_data(
         self,
@@ -82,8 +105,8 @@ class Generator:
         table_name: str,
         trading_share: bool,
         trading_trend: bool,
-        start_date: date,
-        end_date: date,
+        start_date: datetime.date,
+        end_date: datetime.date,
     ) -> dict:
 
         past_universe_stock_list = self.get_past_universe_stock_list(universe)
@@ -92,14 +115,14 @@ class Generator:
 
         query = MySQLQuery.from_(data_day_price).join(data_ticker).on(data_day_price.ticker_id == data_ticker.id)
         if trading_share is True:
-            data_share = Table("data_daytradinginfo")
+            data_share = Table(PhysicalTable.day_trading_info.value)
             query = query.join(data_share).on(
                 Criterion.all(
                     [data_day_price.ticker_id == data_share.ticker_id, data_day_price.date == data_share.date]
                 )
             )
         if trading_trend is True:
-            data_trend = Table("data_daytradingtrend")
+            data_trend = Table(PhysicalTable.day_trading_trend.value)
             query = query.left_join(data_trend).on(
                 Criterion.all(
                     [data_day_price.ticker_id == data_trend.ticker_id, data_day_price.date == data_trend.date]
@@ -151,7 +174,7 @@ class Generator:
         return df
 
     def get_day_price_data(
-        self, universe: Universe, trading_share: bool, trading_trend: bool, start_date: date, end_date: date
+        self, universe: Universe, trading_share: bool, trading_trend: bool, start_date: datetime.date, end_date: datetime.date
     ) -> dict:
 
         """
@@ -164,7 +187,7 @@ class Generator:
         :return: 일봉 데이터
         """
         return self.__get_day_price_data(
-            universe, TABLE_RAW_CANDLE_DAY, trading_share, trading_trend, start_date, end_date
+            universe, PhysicalTable.candle_day.value, trading_share, trading_trend, start_date, end_date
         )
 
     def get_edited_day_price_data(self, universe: Universe) -> dict:
@@ -173,24 +196,24 @@ class Generator:
         :param universe:
         :return: 일봉 데이터
         """
-        return self.__get_day_price_data(universe, TABLE_EDITED_CANDLE_DAY)
+        return self.__get_day_price_data(universe, PhysicalTable.edited_candle_day)
 
     def __get_day_price_data_by_ticker(
-        self, ticker, table_name, trading_share: bool, trading_trend: bool, start_date: date, end_date: date
+        self, ticker, table_name, trading_share: bool, trading_trend: bool, start_date: datetime.date, end_date: datetime.date
     ):
 
         data_day_price, data_ticker = Tables(table_name, "data_ticker")
 
         query = MySQLQuery.from_(data_day_price).join(data_ticker).on(data_day_price.ticker_id == data_ticker.id)
         if trading_share is True:
-            data_share = Table("data_daytradinginfo")
+            data_share = Table(PhysicalTable.day_trading_info.value)
             query = query.join(data_share).on(
                 Criterion.all(
                     [data_day_price.ticker_id == data_share.ticker_id, data_day_price.date == data_share.date]
                 )
             )
         if trading_trend is True:
-            data_trend = Table("data_daytradingtrend")
+            data_trend = Table(PhysicalTable.day_trading_trend.value)
             query = query.left_join(data_trend).on(
                 Criterion.all(
                     [data_day_price.ticker_id == data_trend.ticker_id, data_day_price.date == data_trend.date]
@@ -236,7 +259,7 @@ class Generator:
         return df
 
     def get_day_price_data_by_ticker(
-        self, ticker, trading_share: bool, trading_trend: bool, start_date: date, end_date: date
+        self, ticker, trading_share: bool, trading_trend: bool, start_date: datetime.date, end_date: datetime.date
     ):
         """
         universe에 포함된 종목들의 수정 주가 일봉 데이터 반환
@@ -248,7 +271,7 @@ class Generator:
         :return: 해당 ticker의 일봉 데이터
         """
         return self.__get_day_price_data_by_ticker(
-            ticker, TABLE_RAW_CANDLE_DAY, trading_share, trading_trend, start_date, end_date
+            ticker, PhysicalTable.candle_day, trading_share, trading_trend, start_date, end_date
         )
 
     def get_edited_day_price_data(self, ticker):
@@ -257,47 +280,37 @@ class Generator:
         :param ticker: 개별 종목의 ticker
         :return: 해당 ticker의 일봉 데이터
         """
-        return self.__get_edited_day_price_data_by_ticker(ticker, TABLE_EDITED_CANDLE_DAY)
+        return self.__get_edited_day_price_data_by_ticker(ticker, PhysicalTable.edited_candle_day)
 
-    def get_today_universe_stock_list(self, universe: Universe, today: date) -> list:
+    def get_today_universe_stock_list(self, universe: Universe, today: datetime.date) -> list:
         """
         당일의 universe에 포함된 종목 리스트 반환
         :param universe: kospi, kosdaq, kospi200, kosdaq150, top2000, top350(kospi200과 kosdaq150)
         :param today: 당일 날짜
         :return: 해당 universe의 종목 리스트
         """
-        if universe in (Universe.kospi, Universe.kosdaq, Universe.kospi200, Universe.kosdaq150):
+        if universe in (Universe.kospi, Universe.kosdaq):
             data_universe = Table(f"data_{universe.name}history")
             query = MySQLQuery.from_(data_universe).select(data_universe.tickers).where(data_universe.date == today)
             df = self.executor.sql(query.get_sql())
             today_universe_stock_list = df["tickers"].iloc[0].split(",")
-        elif universe in (Universe.total, Universe.top350):
-            if universe == Universe.total:
-                today_universe_stock_list = self.get_today_universe_stock_list(
-                    Universe.kospi, today
-                ) + self.get_today_universe_stock_list(Universe.kosdaq, today)
-            elif universe == Universe.top350:
-                today_universe_stock_list = self.get_today_universe_stock_list(
-                    Universe.kospi200, today
-                ) + self.get_today_universe_stock_list(Universe.kosdaq150, today)
+        else:
+            today_universe_stock_list = self.get_today_universe_stock_list(
+                Universe.kospi, today
+            ) + self.get_today_universe_stock_list(Universe.kosdaq, today)
 
         return today_universe_stock_list
 
-    def get_index_day_price_data(self, universe: Universe, start_date: date, end_date: date) -> Optional[DataFrame]:
+    def get_index_day_price_data(self, universe: Universe, start_date: datetime.date, end_date: datetime.date) -> Optional[DataFrame]:
         """
         해당 universe의 index 가격을 조회
-        :param universe: kospi, kosdaq, kospi200, kosdaq150
+        :param universe: kospi, kosdaq
         :param start_date:
         :param end_date:
         :return:
         """
         if universe in (Universe.kospi, Universe.kosdaq):
             ticker = universe.name.upper()
-        elif universe in (Universe.kospi200, Universe.kosdaq150):
-            if universe == Universe.kospi200:
-                ticker = "KOSPI200"
-            elif universe == Universe.kosdaq150:
-                ticker = "KOSDAQ150"
 
         data_index_candle = Table("data_indexcandleday")
         query = (
@@ -318,7 +331,7 @@ class Generator:
 
         return df
 
-    def get_exchange_rate(self, exchange_index: ExchangeRate, start_date: date, end_date: date) -> DataFrame:
+    def get_exchange_rate(self, exchange_index: ExchangeRate, start_date: datetime.date, end_date: datetime.date) -> DataFrame:
         """
         달러, 유로, 엔 환율 조회
         :param exchange_index:
@@ -326,7 +339,7 @@ class Generator:
         :param end_date:
         :return:
         """
-        data_exchange_rate = Table("data_exchangeratecandleday")
+        data_exchange_rate = Table(PhysicalTable.exchange_rate_candle_day.value)
         query = (
             MySQLQuery.from_(data_exchange_rate)
             .select("*")
