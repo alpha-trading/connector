3c10b59d4e9beca3d63595b176baf75e7903a23c
yangroro <yang@heechan.kr>
2021-05-01 16:14:10 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name="utils",
-    version="5.2.1",
+    version="5.2.2",
     description="Alpha trading utils with pandas",
     python_requires="==3.*,>=3.7.9",
     project_urls={
